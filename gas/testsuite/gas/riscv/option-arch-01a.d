5384cbfd94a16086bf86874aa222d4d0529f4d65
Tsukasa OI <research_trasio@irq.a4lg.com>
2021-12-23 18:22:35 +0900

    RISC-V: Make testcase indentation consistent
    This commit makes some indentation used in testcase regular expressions
    "[(n*SPACE)(1*TAB)]+" consistent "[(1*SPACE)(1*TAB)]+" unless changing
    the pattern would create unaligned lines.
    
    gas/ChangeLog:
    
    	* testsuite/gas/riscv/option-arch-01a.d: Make indentation
    	consistent.
    	* testsuite/gas/riscv/priv-reg-version-1p9p1.d: Likewise.
    	* testsuite/gas/riscv/priv-reg-version-1p10.d: Likewise.
    	* testsuite/gas/riscv/priv-reg-version-1p11.d: Likewise.

--- gas/testsuite/gas/riscv/option-arch-01a.d
+++ gas/testsuite/gas/riscv/option-arch-01a.d
@@ -2,13 +2,13 @@
 #source: option-arch-01.s
 #objdump: -d
 
-.*:[   ]+file format .*
+.*:[ 	]+file format .*
 
 
 Disassembly of section .text:
 
 0+000 <.text>:
-[ 	]+[0-9a-f]+:[  	]+952e[    	]+add[        	]+a0,a0,a1
-[ 	]+[0-9a-f]+:[  	]+00b50533[    	]+add[        	]+a0,a0,a1
-[ 	]+[0-9a-f]+:[  	]+00302573[    	]+csrr[        	]+a0,fcsr
+[ 	]+[0-9a-f]+:[ 	]+952e[ 	]+add[  	]+a0,a0,a1
+[ 	]+[0-9a-f]+:[ 	]+00b50533[ 	]+add[  	]+a0,a0,a1
+[ 	]+[0-9a-f]+:[ 	]+00302573[ 	]+csrr[ 	]+a0,fcsr
 #...
