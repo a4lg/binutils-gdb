7d206d1d5110201cdd705ee883daec201ce5b479
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-06-03 20:13:22 +0900

    RISC-V: Add testcases disassembling zext.h
    This commit adds testcases for zext.h instruction (Zbb extension) on
    diassembler.
    
    gas/ChangeLog:
    
    	* testsuite/gas/riscv/zbb-zext.h-dis-1.s: New test.
    	* testsuite/gas/riscv/zbb-zext.h-dis-2.s: Likewise.
    	* testsuite/gas/riscv/zbb-zext.h-dis-1-1.d: Likewise.
    	* testsuite/gas/riscv/zbb-zext.h-dis-1-2.d: Likewise.
    	* testsuite/gas/riscv/zbb-zext.h-dis-1-3.d: Likewise.
    	* testsuite/gas/riscv/zbb-zext.h-dis-2.d: Likewise.

---
+++ gas/testsuite/gas/riscv/zbb-zext.h-dis-2.d
@@ -0,0 +1,11 @@
+#as: -march=rv32i_zbkb
+#source: zbb-zext.h-dis-2.s
+#objdump: -d
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+
+0+000 <target>:
+[ 	]+[0-9a-f]+:[ 	]+0805c533[ 	]+pack[ 	]+a0,a1,zero
