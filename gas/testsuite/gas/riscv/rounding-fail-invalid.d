9675cd66ba8efe89c58d0ccb9c44beccc19cfb77
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-07-01 11:56:45 +0900

    RISC-V: Reorganize invalid rounding mode test
    This commit reorganizes and adds testcases to invalid rounding mode
    operand test.  It also fixes a typo in the filename.
    
    gas/ChangeLog:
    
    	* testsuite/gas/riscv/rounding-fail-invalid.d: Rename from
    	rouding-fail. Add some testcases.
    	* testsuite/gas/riscv/rounding-fail-invalid.s: Likewise.
    	* testsuite/gas/riscv/rounding-fail-invalid.l: Likewise.

---
+++ gas/testsuite/gas/riscv/rounding-fail-invalid.d
@@ -0,0 +1,3 @@
+#as: -march=rv32ifd
+#source: rounding-fail-invalid.s
+#error_output: rounding-fail-invalid.l
---
+++ gas/testsuite/gas/riscv/rounding-fail-invalid.s
@@ -0,0 +1,5 @@
+target:
+	fadd.s fa1,fa1,fa1,
+	fadd.d fa1,fa1,fa1,
+	fadd.s fa1,fa1,fa1,unknown
+	fadd.d fa1,fa1,fa1,unknown
