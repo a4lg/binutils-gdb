bf3cbf5962fb82235afa7baa0c96e7641a8fc083
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-07-06 11:11:53 +0900

    RISC-V: Non-standard encodings of fence.i (disasm)
    This commit adds disassembler support for non-standard encodings of the
    fence.i instruction.  Those fields are supposed to be zero but base
    implementation shall ignore those fields.
    
    gas/ChangeLog:
    
    	* config/tc-riscv.c (validate_riscv_insn): Add ignored bit fields.
    	(riscv_ip): Add support for ignored bit fields.
    	* testsuite/gas/riscv/zifencei-dis-nonstd.s: New disasm test.
    	* testsuite/gas/riscv/zifencei-dis-nonstd.d: Likewise.
    
    opcodes/ChangeLog:
    
    	* riscv-dis.c (print_insn_args): Add support for ignored bit
    	fields.
    	* riscv-opc.c (riscv_opcodes): Ignore RD/RS1/immediate (I-type)
    	fields on the fence.i instruction.

--- opcodes/riscv-dis.c
+++ opcodes/riscv-dis.c
@@ -226,7 +226,8 @@ print_insn_args (const char *oparg, insn_t l, bfd_vma pc, disassemble_info *info
   fprintf_styled_ftype print = info->fprintf_styled_func;
   const char *opargStart;
 
-  if (*oparg != '\0')
+#define OPARG_IS_END(offset) (oparg[offset] == '\0' || oparg[offset] == '\\')
+  if (!OPARG_IS_END(0))
     print (info->stream, dis_style_text, "\t");
 
   for (; *oparg != '\0'; oparg++)
@@ -425,7 +426,7 @@ print_insn_args (const char *oparg, insn_t l, bfd_vma pc, disassemble_info *info
 
 	case '0':
 	  /* Only print constant 0 if it is the last argument.  */
-	  if (!oparg[1])
+	  if (OPARG_IS_END(1))
 	    print (info->stream, dis_style_immediate, "0");
 	  break;
 
@@ -695,6 +696,9 @@ print_insn_args (const char *oparg, insn_t l, bfd_vma pc, disassemble_info *info
 	      goto undefined_modifier;
 	    }
 	  break;
+
+	case '\\': /* Ignored fields.  */
+	  return;
 
 	default:
 	undefined_modifier:
