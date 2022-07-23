12a8545141c51d72cb75901dac1ebaed9375c966
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-07-23 14:13:15 +0900

    RISC-V: Use static xlen on ADDIW sequence
    Because XLEN for the disassembler is computed and stored in the `xlen'
    variable, this commit replaces uses of `info->mach' with `xlen'
    (when testing for ADDIW / C.ADDIW address sequence).
    
    Not just we used two ways to determine current XLEN `info->mach' and `xlen',
    `xlen' is going to be more important in the future commits.
    
    opcodes/ChangeLog:
    
    	* riscv-dis.c (print_insn_args): Use `xlen' to determine whether
    	XLEN is larger than 32.

--- opcodes/riscv-dis.c
+++ opcodes/riscv-dis.c
@@ -260,7 +260,7 @@ print_insn_args (const char *oparg, insn_t l, bfd_vma pc, disassemble_info *info
 	    case 'j':
 	      if (((l & MASK_C_ADDI) == MATCH_C_ADDI) && rd != 0)
 		maybe_print_address (pd, rd, EXTRACT_CITYPE_IMM (l), false);
-	      if (info->mach == bfd_mach_riscv64
+	      if (xlen > 32
 		  && ((l & MASK_C_ADDIW) == MATCH_C_ADDIW) && rd != 0)
 		maybe_print_address (pd, rd, EXTRACT_CITYPE_IMM (l), true);
 	      print (info->stream, dis_style_immediate, "%d",
@@ -458,7 +458,7 @@ print_insn_args (const char *oparg, insn_t l, bfd_vma pc, disassemble_info *info
 	  if (((l & MASK_ADDI) == MATCH_ADDI && rs1 != 0)
 	      || (l & MASK_JALR) == MATCH_JALR)
 	    maybe_print_address (pd, rs1, EXTRACT_ITYPE_IMM (l), false);
-	  if (info->mach == bfd_mach_riscv64
+	  if (xlen > 32
 	      && ((l & MASK_ADDIW) == MATCH_ADDIW) && rs1 != 0)
 	    maybe_print_address (pd, rs1, EXTRACT_ITYPE_IMM (l), true);
 	  print (info->stream, dis_style_immediate, "%d",
