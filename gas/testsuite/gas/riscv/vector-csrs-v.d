ab30ffc84798c05dde3c3b625fd18b6887dc0b7d
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-09-05 07:54:51 +0000

    RISC-V: Fix vector CSR requirements
    Vector CSRs are also required on smaller vector subsets.
    
    Not only that the most of vector CSRs are general purpose (and must be
    accessible for every vector subsets), current minimum vector subset 'Zve32x'
    requires fixed point arithmetic, making remaining non-general purpose
    (fixed point arithmetic only) CSRs mandatory for such subsets.
    
    So, those CSRs must be accessible from 'Zve32x', not just from 'V'.
    This commit fixes this issue which caused CSR accessibility warnings.
    
    gas/ChangeLog:
    
    	* config/tc-riscv.c (riscv_csr_address): Change vector CSR
    	requirement from 'V' to 'Zve32x'.
    	* testsuite/gas/riscv/csr-version-1p9p1.l: Change vector CSR
    	requirement from 'V' to 'Zve32x'.
    	* testsuite/gas/riscv/csr-version-1p10.l: Likewise.
    	* testsuite/gas/riscv/csr-version-1p11.l: Likewise.
    	* testsuite/gas/riscv/csr-version-1p12.l: Likewise.
    	* testsuite/gas/riscv/vector-csrs.s: New test.
    	* testsuite/gas/riscv/vector-csrs-v.d: Likewise.
    	* testsuite/gas/riscv/vector-csrs-zve32x.d: Likewise.
    	* testsuite/gas/riscv/vector-csrs-zve32f.d: Likewise.
    	* testsuite/gas/riscv/vector-csrs-zve64x.d: Likewise.
    	* testsuite/gas/riscv/vector-csrs-zve64f.d: Likewise.
    	* testsuite/gas/riscv/vector-csrs-zve64d.d: Likewise.

---
+++ gas/testsuite/gas/riscv/vector-csrs-v.d
@@ -0,0 +1,21 @@
+#as: -march=rv32iv -mcsr-check
+#source: vector-csrs.s
+#objdump: -d
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+
+0+000 <target>:
+[ 	]+[0-9a-f]+:[ 	]+00802573[ 	]+csrr[ 	]+a0,vstart
+[ 	]+[0-9a-f]+:[ 	]+00801073[ 	]+csrw[ 	]+vstart,zero
+[ 	]+[0-9a-f]+:[ 	]+00902573[ 	]+csrr[ 	]+a0,vxsat
+[ 	]+[0-9a-f]+:[ 	]+0090d073[ 	]+csrwi[ 	]+vxsat,1
+[ 	]+[0-9a-f]+:[ 	]+00a02573[ 	]+csrr[ 	]+a0,vxrm
+[ 	]+[0-9a-f]+:[ 	]+00a1d073[ 	]+csrwi[ 	]+vxrm,3
+[ 	]+[0-9a-f]+:[ 	]+00f02573[ 	]+csrr[ 	]+a0,vcsr
+[ 	]+[0-9a-f]+:[ 	]+00f3d073[ 	]+csrwi[ 	]+vcsr,7
+[ 	]+[0-9a-f]+:[ 	]+c2002573[ 	]+csrr[ 	]+a0,vl
+[ 	]+[0-9a-f]+:[ 	]+c2102573[ 	]+csrr[ 	]+a0,vtype
+[ 	]+[0-9a-f]+:[ 	]+c2202573[ 	]+csrr[ 	]+a0,vlenb
