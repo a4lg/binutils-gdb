6fbb196de8e94471c14ab78c82bd5d2fd7667c17
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-09-29 09:47:24 +0000

    UNRATIFIED RISC-V: Add 'Zbpbo' ISA extension support
    [DO NOT MERGE]
    Until 'P' extension and its subextensions are frozen/ratified and final
    version number is determined, this patch should not be merged upstream.
    This commit uses unratified version 0.9 as in the documentation
    (version 0.9.11).
    
    This commit adds 'Zbpbo' ISA extension instructions.
    
    This is based on:
    <https://github.com/riscv/riscv-p-spec/commit/5a12c90b2c206c501a4489eb79e5d4d46afa1014>,
    version 0.9.11 release of the 'P'-extension proposal draft.
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c (riscv_multi_subset_supports): Add handling for
    	new instruction classes.
    	(riscv_multi_subset_supports_ext): Likewise.
    
    gas/ChangeLog:
    
    	* config/tc-riscv.c (riscv_ip): Add extension variant handling
    	related to CLZ instruction.
    	* testsuite/gas/riscv/zbpbo.s: New test.
    	* testsuite/gas/riscv/zbpbo-32.d: Likewise.
    	* testsuite/gas/riscv/zbpbo-64.d: Likewise.
    	* testsuite/gas/riscv/zbpbo-32-noalias.d: Likewise.
    	* testsuite/gas/riscv/zbpbo-fail-imm.s: New failure test.
    	* testsuite/gas/riscv/zbpbo-fail-imm.d: Likewise.
    	* testsuite/gas/riscv/zbpbo-fail-imm.l: Likewise.
    
    include/ChangeLog:
    
    	* opcode/riscv-opc.h (MATCH_CMIX, MASK_CMIX, MATCH_FSR, MASK_FSR,
    	MATCH_FSRI, MASK_FSRI, MATCH_FSRW, MASK_FSRW, MATCH_PACKU,
    	MASK_PACKU): New.
    	* opcode/riscv.h (enum riscv_insn_class): Add new instruction
    	classes INSN_CLASS_ZBPBO, INSN_CLASS_ZBB_OR_ZBPBO and
    	INSN_CLASS_ZBKB_OR_ZBPBO.
    
    opcodes/ChangeLog:
    
    	* riscv-dis.c (print_insn_args): Add handling for 'r', RS3 using
    	general purpose registers.
    	* riscv-opc.c (MATCH_SHAMT_REV_32, MATCH_SHAMT_REV_64): New macro
    	to implement "rev" instruction.
    	(MATCH_SHAMT_REV8_H): New macro to implement "rev8.h" instruction.

---
+++ gas/testsuite/gas/riscv/zbpbo-32-noalias.d
@@ -0,0 +1,23 @@
+#as: -march=rv32i_zbpbo -defsym rv32=1
+#source: zbpbo.s
+#objdump: -d -M no-aliases
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+
+0+000 <target>:
+[ 	]+[0-9a-f]+:[ 	]+08c5c533[ 	]+pack[ 	]+a0,a1,a2
+[ 	]+[0-9a-f]+:[ 	]+48c5c533[ 	]+packu[ 	]+a0,a1,a2
+[ 	]+[0-9a-f]+:[ 	]+0ac5e533[ 	]+max[ 		]+a0,a1,a2
+[ 	]+[0-9a-f]+:[ 	]+0ac5c533[ 	]+min[ 		]+a0,a1,a2
+[ 	]+[0-9a-f]+:[ 	]+6885d513[ 	]+rev8\.h[ 	]+a0,a1
+[ 	]+[0-9a-f]+:[ 	]+6ec59533[ 	]+cmix[ 	]+a0,a2,a1,a3
+[ 	]+[0-9a-f]+:[ 	]+69f5d513[ 	]+rev[ 		]+a0,a1
+[ 	]+[0-9a-f]+:[ 	]+60059513[ 	]+clz[ 		]+a0,a1
+[ 	]+[0-9a-f]+:[ 	]+6cc5d533[ 	]+fsr[ 		]+a0,a1,a3,a2
+[ 	]+[0-9a-f]+:[ 	]+6c05d513[ 	]+fsri[ 	]+a0,a1,a3,0x0
+[ 	]+[0-9a-f]+:[ 	]+6ff5d513[ 	]+fsri[ 	]+a0,a1,a3,0x3f
+[ 	]+[0-9a-f]+:[ 	]+6ec59533[ 	]+cmix[ 	]+a0,a2,a1,a3
+[ 	]+[0-9a-f]+:[ 	]+6885d513[ 	]+rev8\.h[ 	]+a0,a1
