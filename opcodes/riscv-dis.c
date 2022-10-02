f21749719c94fc35e2b68ccecfe746eed8a7b6f6
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-10-02 05:57:30 +0000

    RISC-V: Add "XUN@S" operand type
    This is a variant of operand type "XuN@S" but when disassembling, it's
    printed as a hexadecimal number.
    
    The author intends to use this operand type on:
    
    -   Shift amount operands on 'P'-extension proposal's shift instructions
        (to make them consistent with regular shift instructions)
    -   Landing pad label operand on the 'Zisslpcfi' extension proposal
        (because they allow three different precision of landing pad label
        [9, 17 and 25-bits] with up to three likely consecutive instructions
        with 9, 8 and 8-bit immediates respectively, printing them as binary-
        based will fit better to these instructions)
    
    gas/ChangeLog:
    
    	* config/tc-riscv.c (validate_riscv_insn, riscv_ip): Add new
    	operand type and its handling.
    
    opcodes/ChangeLog:
    
    	* riscv-dis.c (print_insn_args): Print new operand type value
    	as a hexadecimal number.

--- opcodes/riscv-dis.c
+++ opcodes/riscv-dis.c
@@ -587,8 +587,9 @@ print_insn_args (const char *oparg, insn_t l, bfd_vma pc, disassemble_info *info
 	    size_t n;
 	    size_t s;
 	    bool sign;
+	    char opch = *++oparg;
 
-	    switch (*++oparg)
+	    switch (opch)
 	      {
 		case 'l': /* Literal.  */
 		  oparg++;
@@ -603,6 +604,7 @@ print_insn_args (const char *oparg, insn_t l, bfd_vma pc, disassemble_info *info
 		  sign = true;
 		  goto print_imm;
 		case 'u': /* 'XuN@S' ... N-bit unsigned immediate at bit S.  */
+		case 'U': /* 'XUN@S' ... same but disassembled as hex.  */
 		  sign = false;
 		  goto print_imm;
 		print_imm:
@@ -613,8 +615,9 @@ print_insn_args (const char *oparg, insn_t l, bfd_vma pc, disassemble_info *info
 		  oparg--;
 
 		  if (!sign)
-		    print (info->stream, dis_style_immediate, "%lu",
-			   (unsigned long)EXTRACT_U_IMM (n, s, l));
+		    print (info->stream, dis_style_immediate,
+			   opch == 'U' ? "0x%lx" : "%lu",
+			   (unsigned long) EXTRACT_U_IMM (n, s, l));
 		  else
 		    print (info->stream, dis_style_immediate, "%li",
 			   (signed long)EXTRACT_S_IMM (n, s, l));
