aec3d14d4a100cd6f33b2ac4193d19eb48eb1e88
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-10-25 09:08:09 +0000

    TEST: Add instantiation script on CSR allocation
    **DEVELOPMENT ENVIRONMENT ONLY**
    
    The attached script can be used to replace related source files when
    the CSR addresses of "spmpcfg*", "spmpaddr*" and "spmpswitch[01]"
    (now tentative) are determined.
    
    Note that this script accepts the CSR addresses of "spmpcfg0" and
    "spmpaddr0", assuming subsequent entries are consecutive.

---
+++ instantiate-sspmp.sh
@@ -0,0 +1,130 @@
+#! /bin/bash
+# SPDX-License-Identifier: CC0-1.0
+# Author: Tsukasa OI <research_trasio@irq.a4lg.com>
+# Year: 2023
+
+check_csr_addr ()
+{
+	ADDR="$1"
+	case "$ADDR" in
+	0x* | 0X*)
+		ADDR="${ADDR#??}"
+		;;
+	esac
+	case "$ADDR" in
+	[0-9a-fA-F])
+		ADDR=00$ADDR
+		;;
+	[0-9a-fA-F][0-9a-fA-F])
+		ADDR=0$ADDR
+		;;
+	[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])
+		;;
+	*)
+		echo "ERROR: \`$1' is not a valid CSR address for \`$2'." 1>&2
+		exit 1
+		;;
+	esac
+	ADDR=$(echo $ADDR | tr A-F a-f)
+	printf %s $ADDR
+	return 0
+}
+
+canonicalize_hex ()
+{
+	ADDR=$1
+	while :
+	do
+		case $ADDR in
+		0)
+			break
+			;;
+		0?*)
+			ADDR=${ADDR#0}
+			;;
+		*)
+			break
+			;;
+		esac
+	done
+	printf 0x%s $ADDR
+}
+
+dwarf_regnum ()
+{
+	echo -n $(($1 + 4096))
+}
+
+dwarf_offset ()
+{
+	echo -n $(($1 * 4))
+}
+
+test_path ()
+{
+	if test '!' -f "$1"
+	then
+		echo "ERROR: file \`$1' is not found." 1>&2
+		exit 1
+	fi
+}
+
+if test $# -ne 4
+then
+	ERRMSG="usage: $0 ADDR_SPMPCFG0 ADDR_SPMPADDR0 ADDR_SPMPSWITCH0 ADDR_SPMPSWITCH1"
+	echo "$ERRMSG" 1>&2
+	exit 1
+fi
+
+PRIV_VERSIONS="1p9p1 1p10 1p11 1p12"
+
+test_path include/opcode/riscv-opc.h
+for V in $PRIV_VERSIONS
+do
+	test_path gas/testsuite/gas/riscv/csr-version-$V.d
+done
+test_path gas/testsuite/gas/riscv/csr-dw-regnums.s
+test_path gas/testsuite/gas/riscv/csr-dw-regnums.d
+
+do_csr_instantiation ()
+{
+	CSR="$1"
+	CSR_UPPER=$(echo $CSR | tr a-z A-Z)
+	ADDR=$(check_csr_addr "$2" $CSR)
+	HEXC=$(canonicalize_hex $ADDR)
+	DWREG=$(dwarf_regnum $HEXC)
+	DWOFF=$(dwarf_offset $HEXC)
+	ORIG_ADDR=$(grep "a0,${CSR}\$" gas/testsuite/gas/riscv/csr-version-1p12.d | head -n 1 | sed 's/.*+\([0-9a-f]\{3\}\)02573.*/\1/')
+	sed -i "s/^#define CSR_${CSR_UPPER} .*/#define CSR_${CSR_UPPER} $HEXC/" include/opcode/riscv-opc.h
+	for V in $PRIV_VERSIONS
+	do
+		sed -i "s/+${ORIG_ADDR}\\(02573\\|59073\\)/+${ADDR}\\1/" gas/testsuite/gas/riscv/csr-version-$V.d
+	done
+	sed -i "s/\\.cfi_offset ${CSR}, .*/.cfi_offset ${CSR}, ${DWOFF}/" gas/testsuite/gas/riscv/csr-dw-regnums.s
+	sed -i "s/DW_CFA_offset_extended_sf: r[0-9]\\+ \\\\(${CSR}\\\\) at cfa\\\\+[0-9]\\+/DW_CFA_offset_extended_sf: r${DWREG} \\\\(${CSR}\\\\) at cfa\\\\+${DWOFF}/" gas/testsuite/gas/riscv/csr-dw-regnums.d
+}
+
+ADDR=$(check_csr_addr "$1" spmpcfg0)
+NADDR=$((0x$ADDR))
+for N in $(seq 0 15)
+do
+	ADDR_N=$(printf %03x $(($NADDR + $N)))
+	do_csr_instantiation spmpcfg$N $ADDR_N
+done
+
+ADDR=$(check_csr_addr "$2" spmpaddr0)
+NADDR=$((0x$ADDR))
+for N in $(seq 0 63)
+do
+	ADDR_N=$(printf %03x $(($NADDR + $N)))
+	do_csr_instantiation spmpaddr$N $ADDR_N
+done
+
+do_csr_instantiation spmpswitch0 "$3"
+do_csr_instantiation spmpswitch1 "$4"
+
+if test -f "$0"
+then
+	rm -f "$0"
+fi
+exit 0
