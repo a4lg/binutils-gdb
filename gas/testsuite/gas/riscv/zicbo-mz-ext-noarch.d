569bc0329a0edd90ee3eba0d985e90c64904b74e
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-11-02 11:25:56 +0000

    RISC-V: Combine/enhance 'Zicbo[mz]' extension tests
    This commit combines tests for 'Zicbom' and 'Zicboz' extensions and adds
    "no required extension" testcases based on new test utilities.  It also
    contains minor tidying (such as minimizing base from RV64G to RV32I).
    
    gas/ChangeLog:
    
    	* testsuite/gas/riscv/zicbo-mz-ext.s: Combine zicbo[mz].s.
    	* testsuite/gas/riscv/zicbo-mz-ext.d: Likewise.
    	Minimize extension requirements and remove source.
    	* testsuite/gas/riscv/zicbo-mz-ext-noarch.d: New test for
    	architecture failure.
    	* testsuite/gas/riscv/zicbo-mz-ext-noarch.l: Likewise.
    	* testsuite/gas/riscv/zicbo-mz-ext-fail.s: Combine
    	zicbo[mz]-fail.s.
    	* testsuite/gas/riscv/zicbo-mz-ext-fail.d: Likewise.
    	Minimize extension requirements.
    	* testsuite/gas/riscv/zicbo-mz-ext-fail.l: Likewise.
    	Make matching pattern stricter.
    	* testsuite/gas/riscv/zicbom.s: Removed.
    	* testsuite/gas/riscv/zicbom.d: Removed.
    	* testsuite/gas/riscv/zicbom-fail.s: Removed.
    	* testsuite/gas/riscv/zicbom-fail.d: Removed.
    	* testsuite/gas/riscv/zicbom-fail.l: Removed.
    	* testsuite/gas/riscv/zicboz.s: Removed.
    	* testsuite/gas/riscv/zicboz.d: Removed.
    	* testsuite/gas/riscv/zicboz-fail.s: Removed.
    	* testsuite/gas/riscv/zicboz-fail.d: Removed.
    	* testsuite/gas/riscv/zicboz-fail.l: Removed.

---
+++ gas/testsuite/gas/riscv/zicbo-mz-ext-noarch.d
@@ -0,0 +1,3 @@
+#as: -march=rv32i -I$srcdir/$subdir -defsym NOARCH=1
+#source: zicbo-mz-ext.s
+#error_output: zicbo-mz-ext-noarch.l
--- gas/testsuite/gas/riscv/zicbo-mz-ext.d
+++ gas/testsuite/gas/riscv/zicbo-mz-ext.d
@@ -1,6 +1,5 @@
-#as: -march=rv64g_zicbom
-#source: zicbom.s
-#objdump: -dr
+#as: -march=rv32i -I$srcdir/$subdir
+#objdump: -d
 
 .*:[ 	]+file format .*
 
@@ -13,3 +12,7 @@ Disassembly of section .text:
 [ 	]+[0-9a-f]+:[ 	]+002f200f[ 	]+cbo\.flush[ 	]+\(t5\)
 [ 	]+[0-9a-f]+:[ 	]+0000a00f[ 	]+cbo\.inval[ 	]+\(ra\)
 [ 	]+[0-9a-f]+:[ 	]+000f200f[ 	]+cbo\.inval[ 	]+\(t5\)
+[ 	]+[0-9a-f]+:[ 	]+0040a00f[ 	]+cbo\.zero[ 	]+\(ra\)
+[ 	]+[0-9a-f]+:[ 	]+004f200f[ 	]+cbo\.zero[ 	]+\(t5\)
+[ 	]+[0-9a-f]+:[ 	]+0040a00f[ 	]+cbo\.zero[ 	]+\(ra\)
+[ 	]+[0-9a-f]+:[ 	]+004f200f[ 	]+cbo\.zero[ 	]+\(t5\)
