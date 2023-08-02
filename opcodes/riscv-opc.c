d076022bf743db7123138792c090c1356075271c
Tsukasa OI <research_trasio@irq.a4lg.com>
2023-08-02 04:08:08 +0000

    UNRATIFIED RISC-V: Add 'Zvfbfwma' extension
    [DO NOT MERGE]
    Until RISC-V BF16 extensions are frozen/ratified and the final version
    number is determined, this patch should not be merged upstream. This
    commit uses unratified version 0.8 as in the latest PDF documentation
    (instead of possible 1.0 after ratification).
    
    This commit adds support for the 'Zvfbfwma' extension, the vector BF16
    multiply then FP32 accumlation extension, consisting of two widening
    multiply-accumulate instructions.
    
    This commit is based on the following specification:
    <https://github.com/riscv/riscv-bfloat16/commit/5578e34e15a44e9ad13246072a29f51274b4d999>
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c (riscv_implicit_subsets) Add 'Zvfbfwma'
    	-> 'Zvfbfmin' implication.
    	(riscv_supported_std_z_ext): Add 'Zvfbfwma'.
    	(riscv_multi_subset_supports): Add support to INSN_CLASS_ZVFBFWMA.
    	(riscv_multi_subset_supports_ext): Likewise.
    
    gas/ChangeLog:
    
    	* testsuite/gas/riscv/zvfbfwma.s: New test.
    	* testsuite/gas/riscv/zvfbfwma.d: Likewise.
    
    include/ChangeLog:
    
    	* opcode/riscv-opc.h (MATCH_VFWMACCBF16_VF, MASK_VFWMACCBF16_VF,
    	MATCH_VFWMACCBF16_VV, MASK_VFWMACCBF16_VV) New.
    	* opcode/riscv.h (enum riscv_insn_class): Add new instruction class
    	INSN_CLASS_ZVFBFWMA.
    
    opcodes/ChangeLog:
    
    	* riscv-opc.c (riscv_opcodes): Add 'Zvfbfwma' instructions.

--- opcodes/riscv-opc.c
+++ opcodes/riscv-opc.c
@@ -1942,6 +1942,10 @@ const struct riscv_opcode riscv_opcodes[] =
 {"vfncvtbf16.f.f.w", 0, INSN_CLASS_ZVFBFMIN, "Vd,VtVm", MATCH_VFNCVTBF16_F_F_W, MASK_VFNCVTBF16_F_F_W, match_opcode, 0},
 {"vfwcvtbf16.f.f.v", 0, INSN_CLASS_ZVFBFMIN, "Vd,VtVm", MATCH_VFWCVTBF16_F_F_V, MASK_VFWCVTBF16_F_F_V, match_opcode, 0},
 
+/* Zvfbfwma instructions.  */
+{"vfwmaccbf16.vv",   0, INSN_CLASS_ZVFBFWMA, "Vd,Vt,VsVm", MATCH_VFWMACCBF16_VV, MASK_VFWMACCBF16_VV, match_opcode, 0},
+{"vfwmaccbf16.vf",   0, INSN_CLASS_ZVFBFWMA, "Vd,Vt,SVm", MATCH_VFWMACCBF16_VF, MASK_VFWMACCBF16_VF, match_opcode, 0},
+
 /* Zvkg instructions.  */
 {"vghsh.vv",   0, INSN_CLASS_ZVKG, "Vd,Vt,Vs", MATCH_VGHSH_VV, MASK_VGHSH_VV, match_opcode, 0},
 {"vgmul.vv",   0, INSN_CLASS_ZVKG, "Vd,Vt", MATCH_VGMUL_VV, MASK_VGMUL_VV, match_opcode, 0},
