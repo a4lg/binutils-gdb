3b3d2a5206c86003bf9a17f1abd513c26e882dce
Tsukasa OI <research_trasio@irq.a4lg.com>
2023-08-21 13:51:18 +0000

    RISC-V: Test cases for complex CSR checking
    This commit tests whether the miselect CSR is unlocked by either
    'Smaia' or 'Smcsrind' extension.
    
    gas/ChangeLog:
    
    	* testsuite/gas/riscv/csr-complex.s: New test.
    	* testsuite/gas/riscv/csr-complex-1.d: Likewise.
    	* testsuite/gas/riscv/csr-complex-1.l: Likewise.
    	* testsuite/gas/riscv/csr-complex-2.d: Likewise.
    	* testsuite/gas/riscv/csr-complex-3.d: Likewise.

---
+++ gas/testsuite/gas/riscv/csr-complex-3.d
@@ -0,0 +1,11 @@
+#as: -march=rv64i_zicsr_smcsrind -mcsr-check
+#source: csr-complex.s
+#objdump: -d
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+
+0+000 <target>:
+[ 	]+[0-9a-f]+:[ 	]+35002573[ 	]+csrr[ 	]+a0,miselect
