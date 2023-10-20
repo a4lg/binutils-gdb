a155952b54ab3444c4feed35eb9d22e5d29dab09
Tsukasa OI <research_trasio@irq.a4lg.com>
2023-10-20 03:11:50 +0000

    RISC-V: Add 'Zicntr' and 'Zihpm' support with compatibility measures
    This commit adds support for 'Zicntr' and 'Zihpm' extensions (version 2.0).
    
    However, because GNU Binutils handled those as a part of 'I' and there was
    a time when a ratified specification did split counters from the 'I'
    extension without separate extension names, we need to take care of
    possible compatibility issues.
    
    So, if a 'Zicntr' pseudoinstruction is used without that extension,
    it generates not an error but a warning.
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c (riscv_implicit_subsets): Add implications related
    	to counter extensions.
    	(riscv_supported_std_z_ext): Add 'Zicntr' and 'Zihpm' extensions.
    	Define default versions of 'Zicsr' and 'Zifencei' on the draft ISA
    	because they might be used on the 'E' extension handling.
    	(riscv_is_subset_of_i_2p0): New function.
    	(riscv_parse_add_subset): If a subset of the 'I' extension version
    	2.0 is being added, check the version of 'I' and allow its version
    	unknown when the 'I' extension version is less than 2.1.
    	(riscv_multi_subset_supports, riscv_multi_subset_supports_ext):
    	Add support for the 'Zicntr' extension with compatibility measure.
    
    gas/ChangeLog:
    
    	* config/tc-riscv.c (enum riscv_csr_class): Add new CSR classes
    	corresponding 'Zicntr' and 'Zihpm' extensions.
    	(riscv_csr_address): Add handling for new CSR classes.
    	(riscv_ip): Raise a warning if a 'Zicntr' pseudoinstruction is
    	used without that extension.
    	* testsuite/gas/riscv/csr-insns-pseudo.s: Rename section names
    	to indicate that the extension 'Zicntr' is needed.
    	* testsuite/gas/riscv/csr-insns-pseudo.d: Add "zicntr" to arch.
    	* testsuite/gas/riscv/csr-insns-pseudo-noalias.d: Likewise.
    	* testsuite/gas/riscv/csr-insns-pseudo-zfinx.d: Likewise.
    	* testsuite/gas/riscv/csr-insns-read-only.d: Likewise.
    	* testsuite/gas/riscv/csr-version-1p9p1.d: Specify versions.
    	* testsuite/gas/riscv/csr-version-1p10.d: Likewise.
    	* testsuite/gas/riscv/csr-version-1p11.d: Likewise.
    	* testsuite/gas/riscv/csr-version-1p12.d: Likewise.
    	* testsuite/gas/riscv/csr-version-1p9p1.l: Add warnings regarding
    	'Zicntr' and 'Zihpm' extension recategorization.
    	* testsuite/gas/riscv/csr-version-1p10.l: Likewise.
    	* testsuite/gas/riscv/csr-version-1p11.l: Likewise.
    	* testsuite/gas/riscv/csr-version-1p12.l: Likewise.
    	* testsuite/gas/riscv/march-imply-i2p0-01.d: 'I' version 2.0
    	effectively imples 'Zicsr' and 'Zifencei' but should not be
    	reflected to the expanded architectural string.
    	* testsuite/gas/riscv/march-ok-reorder.d: Use 'I' version 2.1 and
    	use other extensions to test proper ordering.
    	* testsuite/gas/riscv/csr-insns-pseudo-nozicntr.d: New test to
    	see warnings are generated when the 'Zicntr' extension is not
    	specified.
    	* testsuite/gas/riscv/csr-insns-pseudo-nozicntr.l: Likewise.
    
    include/ChangeLog:
    
    	* opcode/riscv-opc.h: Recategorize user counter CSRs.
    	* opcode/riscv.h (enum riscv_insn_class): Add INSN_CLASS_ZICNTR
    	for 'Zicntr' pseudoinstructions.
    
    opcodes/ChangeLog:
    
    	* riscv-opc.c (riscv_opcodes): Recategorize counter
    	pseudoinstructions to the 'Zicntr' extension.

--- gas/config/tc-riscv.c
+++ gas/config/tc-riscv.c
@@ -66,6 +66,10 @@ enum riscv_csr_class
 
   CSR_CLASS_I,
   CSR_CLASS_I_32,	/* rv32 only */
+  CSR_CLASS_ZICNTR,	/* basic hardware perf counter */
+  CSR_CLASS_ZICNTR_32,	/* basic hardware perf counter, rv32 only */
+  CSR_CLASS_ZIHPM,	/* additional hardware perf counter */
+  CSR_CLASS_ZIHPM_32,	/* additional hardware perf counter, rv32 only */
   CSR_CLASS_F,		/* f-ext only */
   CSR_CLASS_ZKR,	/* zkr only */
   CSR_CLASS_V,		/* rvv only */
@@ -1032,6 +1036,18 @@ riscv_csr_address (const char *csr_name,
     case CSR_CLASS_I:
       need_check_version = true;
       extension = "i";
+      break;
+    case CSR_CLASS_ZICNTR_32:
+      is_rv32_only = true;
+      /* Fall through.  */
+    case CSR_CLASS_ZICNTR:
+      extension = "zicntr";
+      break;
+    case CSR_CLASS_ZIHPM_32:
+      is_rv32_only = true;
+      /* Fall through.  */
+    case CSR_CLASS_ZIHPM:
+      extension = "zihpm";
       break;
     case CSR_CLASS_H_32:
       is_rv32_only = true;
@@ -2609,6 +2625,15 @@ riscv_ip (char *str, struct riscv_cl_insn *ip, expressionS *imm_expr,
 			*(asargStart - 1) = save_c;
 		      as_warn (_("read-only CSR is written `%s'"), str);
 		      insn_with_csr = false;
+		    }
+
+		  /* Check if we are using a 'Zicntr' pseudoinstruction
+		     without the 'Zicntr' extension.  */
+		  if (insn->insn_class == INSN_CLASS_ZICNTR
+		      && !riscv_subset_supports (&riscv_rps_as, "zicntr"))
+		    {
+		      as_warn (_("`%s' needs `zicntr' extension"),
+			       insn->name);
 		    }
 
 		  /* The (segmant) load and store with EEW 64 cannot be used
